63b11b5489bae70b7e2e70675952da18af5a6ec2
aync <ync.com>
2022-11-15 15:40:00 +0000

    add prompt train & evaluate

--- run_scripts/vqa/evaluate_vqa_base_allcand.sh
+++ run_scripts/vqa/evaluate_vqa_base_allcand.sh
@@ -10,9 +10,9 @@ bpe_dir=../../utils/BPE
 # val or test
 split=$1
 
-data=../../dataset/vqa_data/vqa_${split}.tsv
-ans2label_file=../../dataset/vqa_data/trainval_ans2label.pkl
-path=../../checkpoints/vqa_base_best.pt
+data=../../vqa_data/vqa_val.tsv
+ans2label_file=../../vqa_data/trainval_ans2label.pkl
+path=vqa_checkpoints/50_0.04_5e-5_480/checkpoint_best.pt
 result_path=../../results/vqa_${split}_allcand
 selected_cols=0,5,2,3,4
 
@@ -21,7 +21,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m torch.distributed.launch --nproc_per_nod
     --path=${path} \
     --user-dir=${user_dir} \
     --task=vqa_gen \
-    --batch-size=8 \
+    --batch-size=1 \
     --log-format=simple --log-interval=10 \
     --seed=7 \
     --gen-subset=${split} \
